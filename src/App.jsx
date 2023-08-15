dfeaf736a82ecdc3302b77816c6332218974c4da
Henrique Mafra <henriquemafra89@gmail.com>
2023-08-15 00:49:38 -0300

    Refactor: Login-Page layout modification

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-import './App.css'
-import SignIn from './components/login-page'
+import LoginPage from './components/login-page';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
 
 function App() {
 
+  const customColors = {
+    primary: {
+      main: '#3750F0', // Cor primária do aplicativo
+    },
+    secondary: {
+      main: '#000000', // Cor secundária do aplicativo
+    },
+  };
+
+  const customTheme = createTheme({
+    palette: {
+      ...customColors,
+    },
+  });
+
   return (
-    <>
-      <SignIn/>
-    </>
+    <ThemeProvider theme={customTheme}>
+      <LoginPage />
+    </ThemeProvider>
   )
 }
 
--- src/components/login-page/index.jsx
+++ src/components/login-page/index.jsx
@@ -13,15 +13,8 @@ import Paper from '@mui/material/Paper';
 import LockOpenRoundedIcon from '@mui/icons-material/LockOpenRounded';
 import Typography from '@mui/material/Typography';
 import Alert from '@mui/material/Alert';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Copyright from '../copyright';
 import backgroundImg from '../../assets/img/LoginPageBg.png';
-
-
-// Cria tema padrão da biblioteca MUI
-const defaultTheme = createTheme();
-
-
 
 // Usa UseState para criar um estado aos campos do form
 export default function LoginPage() {
@@ -71,102 +64,103 @@ export default function LoginPage() {
     };
 
     return (
-        <ThemeProvider theme={defaultTheme}>
-          <Grid container component="main" sx={{ height: '100vh' }}>
+        <Grid container component="main" sx={{ height: '100vh' }}>
             <CssBaseline />
-             {/* Background da página de login */}
+            {/* Background da página de login */}
             <Grid
-              item
-              xs={false}
-              sm={4}
-              md={7}
-              sx={{
-                backgroundImage: `url(${backgroundImg})`,
-                backgroundRepeat: 'no-repeat',
-                backgroundSize: 'cover',
-                backgroundPosition: 'center',
-              }}
-            />
-            <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-              <Box
+                item
+                xs={false}
+                sm={4}
+                md={7}
                 sx={{
-                  my: 8,
-                  mx: 4,
-                  display: 'flex',
-                  flexDirection: 'column',
-                  alignItems: 'center',
+                    backgroundImage: `url(${backgroundImg})`,
+                    backgroundRepeat: 'no-repeat',
+                    backgroundSize: '100% 100%',
+                    backgroundPosition: 'center',
                 }}
-              >
-                <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
-                            <LockOpenRoundedIcon />
-                        </Avatar>
-                        <Typography component="h1" variant="h5">
-                        Medication Management 
-                        <Typography sx={{textAlign: "center"}} component="p">
-                            Insira seus dados de acesso
-                        </Typography>
-                        </Typography>
-                        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-                            <TextField
-                                margin="normal"
-                                required
-                                fullWidth
-                                id="email"
-                                label="Email"
-                                name="email"
-                                autoComplete="email"
-                                autoFocus
-                                onChange={handleChange}
-                            />
-                            <TextField
-                                margin="normal"
-                                required
-                                fullWidth
-                                name="password"
-                                label="Senha"
-                                type="password"
-                                id="password"
-                                autoComplete="current-password"
-                                onChange={handleChange}
-                            />
-                            <FormControlLabel
-                                control={<Checkbox value="remember" color="primary" />}
-                                label="Lembrar-me"
-                            />
-                            <Button
-                                type="submit"
-                                fullWidth
-                                variant="contained"
-                                sx={{ mt: 3, mb: 2 }}
-                            >
-                                Entrar
-                            </Button>
-                            {errorAlert && (
-                                <Alert variant="outlined" severity="error">Ops! Informe um email válido por favor.</Alert>
-                            )}
-                            {errorAlertPassword && (
-                                <Alert variant="outlined" severity="error">Ops! Informe uma senha válida com no mínimo 8 caracteres incluindo letras e números.</Alert>
-                            )}
-    
-                            <Grid container>
-                                <Grid item xs>
-                                    <Link href="#" variant="body2">
-                                        Esqueceu sua senha?
-                                    </Link>
-                                </Grid>
-                                <Grid item>
-                                    <Link href="#" variant="body2">
-                                        {"Cadastre-se"}
-                                    </Link>
-                    </Grid>
-                  </Grid>
-                  
+            />
+            <Grid item xs={12} sm={8} md={5} component={Paper} elevation={24} square>
+                <Box
+                    sx={{
+                        mt: 18,
+                        mx: 4,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}
+                >
+                    <img
+                        style={{ marginBottom: '10px' }}
+                        alt="Logo"
+                        src="src\assets\img\logo.png"
+                        width="300"
+                        height="70"
+                    />
+                    <Typography sx={{ textAlign: "center" }} component="p">
+                        Insira seus dados de acesso
+                    </Typography>
+                    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
+                            onChange={handleChange}
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Senha"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                            onChange={handleChange}
+                        />
+                        <FormControlLabel
+                            control={<Checkbox value="remember" color="primary" />}
+                            label="Lembrar-me"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Entrar
+                        </Button>
+                        {errorAlert && (
+                            <Alert variant="outlined" severity="error">Ops! Informe um email válido por favor.</Alert>
+                        )}
+                        {errorAlertPassword && (
+                            <Alert variant="outlined" severity="error">Ops! Informe uma senha válida com no mínimo 8 caracteres incluindo letras e números.</Alert>
+                        )}
+
+                        <Grid container>
+                            <Grid item xs>
+                                <Link href="#" variant="body2">
+                                    Esqueceu sua senha?
+                                </Link>
+                            </Grid>
+                            <Grid item>
+                                <Link href="#" variant="body2">
+                                    {"Cadastre-se"}
+                                </Link>
+                            </Grid>
+                        </Grid>
+
+                    </Box>
                 </Box>
-              </Box>
-              <Copyright sx={{ mt: 5
-                 }} />
+                <Copyright sx={{
+                    mt: 5
+                }} />
             </Grid>
-          </Grid>
-        </ThemeProvider>
-      );
+        </Grid>
+    );
 }
