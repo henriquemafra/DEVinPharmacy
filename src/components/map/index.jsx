8e5ff5cecc4a6ea47cee0c7e3dba4923ad33dac2
Henrique Mafra <henriquemafra89@gmail.com>
2023-08-21 14:34:59 -0300

    Fix: Leaflet ICON

--- src/components/map/index.jsx
+++ src/components/map/index.jsx
@@ -1,5 +1,6 @@
 import 'leaflet/dist/leaflet.css';
 import markerIconPng from 'leaflet/dist/images/marker-icon.png'
+import {Icon} from 'leaflet'
 import { useState, useEffect, React } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import { Container, Typography, Box, Button} from '@mui/material';
